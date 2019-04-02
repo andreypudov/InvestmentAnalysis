f5367d19d9c338f69ed5005f0190ca80314c5688
Andrey Pudov <andrey.pudov@intel.com>
2019-04-02 18:22:06 +0300

    ~ adds unit test project scheleton for finam provider

---
+++ src/InvestmentAnalysis.Portfolio.Finam.Tests/FinamPortfolio.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Andrey Pudov.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See LICENSE.txt in the project root for license information.
+
+using Xunit;
+
+namespace InvestmentAnalysis.Portfolio.Finam.Tests
+{
+    public class FinamPortfolio
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
